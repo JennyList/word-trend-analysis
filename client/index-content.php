ba0c8f47727170024d5fe494861057ba69b3cd2e
Jenny List <jennyalto@gmail.com>
2025-06-04 16:46:44 +0200

    fixed index links

--- client/index-content.php
+++ client/index-content.php
@@ -5,8 +5,8 @@
 
 Index page content
 <p>
-<a href="./client/index.php/timeline/">Timeline</a><br/>
-<a href="./client/index.php/trends/">Graphs</a><br/>
+<a href="./index.php/timeline/">Timeline</a><br/>
+<a href="./index.php/trends/foo">Graphs</a><br/> <!-- defaults to "foo" as the first word -->
 </p>
 <p>
 /timeline/ - wordcloud of words this day or week<br/>
